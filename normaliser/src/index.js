cdbf849d199b7546b08c7a144723b85726bde1b6
Andy Marks <vampwillow@gmail.com>
2019-03-22 11:38:12 +1100

    Added tests based on Clojure version

--- normaliser/src/index.js
+++ normaliser/src/index.js
@@ -1,9 +1,22 @@
 'use strict';
 
 exports.http = (request, response) => {
-  response.status(200).send('Normaliser!');
+  response.status(200).send(this.normalise(request.url));
 };
+
+
+exports.normalise = (input) => {
+  return stripNonAlphanumeric(stripLeadingPath(input)).toLowerCase()
+}
 
 exports.event = (event, callback) => {
   callback();
 };
+
+function stripNonAlphanumeric(input) {
+  return input.replace(/[^a-z0-9]/gi, "");
+}
+
+function stripLeadingPath(input) {
+  return input.replace(/^\//, "");
+}
--- normaliser/test/test.js
+++ normaliser/test/test.js
@@ -1,8 +1,36 @@
 var assert = require('assert');
+var httpMocks = require('node-mocks-http');
+const fn = require('../src/index');
+
 describe('Normaliser', function () {
+    describe('#normalise()', function () {
+        it('should gracefully handle no input', function () {
+            assert.equal("", fn.normalise("/"));
+            assert.equal("", fn.normalise(""));
+        });
+        it('should make no changes to normal text', function () {
+            assert.equal("abcd1234", fn.normalise("/abcd1234"));
+        });
+        it('should downcase all text', function () {
+            assert.equal("abcde", fn.normalise("/aBcDe"));
+        });
+        it('should remove all spaces', function () {
+            assert.equal("abcd", fn.normalise("/a+b+c+d"));
+        });
+    });
     describe('#http()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal([1, 2, 3].indexOf(4), -1);
+        it('should form a valid response from the request', function () {
+            var request = httpMocks.createRequest({
+                method: 'GET',
+                url: '/abcd',
+            });
+
+            var response = httpMocks.createResponse();
+            var expectedResponse = fn.normalise("/abcd");
+
+            fn.http(request, response);
+            assert.equal(200, response.statusCode);
+            assert.equal(expectedResponse, response._getData());
         });
     });
 });
