b89a527f3587ed2720e674a66d98d3d082208053
Andy Marks <vampwillow@gmail.com>
2019-03-22 12:03:50 +1100

    Added tests based on Clojure version

--- square-sizer/src/index.js
+++ square-sizer/src/index.js
@@ -1,8 +1,16 @@
 'use strict';
 
 exports.http = (request, response) => {
-  response.status(200).send('Square sizer!');
+  response.status(200).send(this.squareSizeFor(request.url));
 };
+
+exports.squareSizeFor = (input) => {
+  return Math.ceil(Math.sqrt(stripLeadingPath(input).length)).toString()
+}
+
+function stripLeadingPath(input) {
+  return input.replace(/^\//, "");
+}
 
 exports.event = (event, callback) => {
   callback();
--- square-sizer/test/test.js
+++ square-sizer/test/test.js
@@ -1,8 +1,29 @@
 var assert = require('assert');
+var httpMocks = require('node-mocks-http');
+const fn = require('../src/index');
+
 describe('Square Sizer', function () {
+    describe("#squareSizeFor()", function () {
+        it("should calculate the size correctly when the input is an exact square", function () {
+            assert.equal(fn.squareSizeFor("/abcd"), 2)
+        });
+        it("should calculate the size correctly when the input is not an exact square", function () {
+            assert.equal(fn.squareSizeFor("/abcde"), 3)
+        });
+    });
     describe('#http()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal([1, 2, 3].indexOf(4), -1);
+        it('should form a valid response from the request', function () {
+            var request = httpMocks.createRequest({
+                method: 'GET',
+                url: '/abcdefgh',
+            });
+
+            var response = httpMocks.createResponse();
+            var expectedResponse = fn.squareSizeFor("/abcdefgh");
+
+            fn.http(request, response);
+            assert.equal(response.statusCode, 200);
+            assert.equal(expectedResponse, response._getData());
         });
     });
 });
